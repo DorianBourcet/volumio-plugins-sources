c6752e224a3d3920f5b0ee5ef1a547362b678ccb
Dorian Bourcet <d.bourcet@gmail.com>
2022-10-09 16:19:25 +0000

    Remove console logs
    Signed-off-by: Dorian Bourcet <d.bourcet@gmail.com>

--- metaradio/helpers/timer.js
+++ metaradio/helpers/timer.js
@@ -9,7 +9,6 @@ function Timer(taskFn, delayFn, initialDelay) {
 
   self.start = function() {
     var self = this;
-    console.log('CALLED_START');
     self.timeoutObj = setTimeout(
       self.executeTask,
       self.latestDelay
@@ -18,11 +17,8 @@ function Timer(taskFn, delayFn, initialDelay) {
 
   self.executeTask = function() {
     var self = this;
-    console.log('TIMER_EXECUTE_TASK');
-    console.log('DELAY '+self.latestDelay);
     self.taskFn().then(function(result) {
       self.latestDelay = delayFn(result);
-      console.log('POMMIER '+self.latestDelay);
       self.timeoutObj = setTimeout(
         self.executeTask,
         self.latestDelay
