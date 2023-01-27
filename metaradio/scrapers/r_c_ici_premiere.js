db3548cab947ecf96bc9ed6ee9bb7c66037a80fc
Dorian Bourcet <soixante6dollars@gmail.com>
2023-01-27 17:24:31 -0500

    Add Radio-Canada stations
    Signed-off-by: Dorian Bourcet <soixante6dollars@gmail.com>

---
+++ metaradio/scrapers/r_c_ici_premiere.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const jp = require('jsonpath');
+const BaseScraper = require('./base');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+
+class RCIciPremiereScraper extends BaseScraper {
+
+  getMetadata(url) {
+    return this._fetchMetadata(url)
+      .then(function (eventResponse) {
+        if (eventResponse !== null) {
+          return JSON.parse(eventResponse);
+        }
+      })
+      .then(function (metadata) {
+        return {
+          title: 'ICI Première Québec'
+        };
+      });
+  }
+
+}
+
+module.exports = RCIciPremiereScraper;
