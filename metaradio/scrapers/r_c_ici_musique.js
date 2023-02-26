aee34adb53dcf4e5d730c2d21640ce3ae26f8ad8
Dorian Bourcet <d.bourcet@gmail.com>
2023-02-26 20:27:07 +0000

    Fix Radio-Canada scraping
    Signed-off-by: Dorian Bourcet <d.bourcet@gmail.com>

--- metaradio/scrapers/r_c_ici_musique.js
+++ metaradio/scrapers/r_c_ici_musique.js
@@ -16,34 +16,77 @@ class RCIciMusiqueScraper extends BaseScraper {
         }
       })
       .then(function (metadata) {
+        var self = this;
+        console.log('ALLO');
         let found = metadata.schedules.find(item => item.broadcastingNetworkId == 4 && item.broadcastingStationId == 83);
-        if (!found) {return {};}
+        if (!found) {console.log('ALLO2');return {};}
+        console.log('ALLO3');
+        //let program = self.findBroadcastingProgram(found.trafficBroadcasts);
         dayjs.extend(utc);
+        console.log('ALLO3-00');
         let now = dayjs().unix();
-        if (found.musicTracks) {
-          let broadcastingTitle = this.findBroadcastingTitle(found.musicTracks);
-        }
-        if (!found.trafficBroadcasts) {return {};}
+        console.log('ALLO3-01');
         let broadcast = found.trafficBroadcasts.find(function(item) {
+          console.log('HEY');
           return dayjs(item.startsAt).unix() <= now && dayjs(item.endsAt).unix() > now;
         });
+        console.log('ALLO3-1');
         if (!broadcast) {return {};}
-        var [title] = jp.query(broadcast, '$.title');
-        var [artist] = jp.query(broadcast, '$.credits');
-        var [cover] = jp.query(broadcast, '$.picture.url');
-        cover = cover.replace('{1}','1x1').replace('{0}','400');
-        var [startTime] = jp.query(broadcast, '$.startsAt');
-        startTime = dayjs(startTime).unix();
-        var [endTime] = jp.query(broadcast, '$.endsAt');
-        endTime = dayjs(endTime).unix();
-        return {
-          title,
-          artist,
-          cover,
-          startTime,
-          endTime,
+        let program = {
+          title: broadcast.title,
+          artist: broadcast.credits,
+          cover: broadcast.picture.url.replace('{1}','1x1').replace('{0}','400'),
+          startTime: dayjs(broadcast.startsAt).unix(),
+          endTime: dayjs(broadcast.endsAt).unix(),
         };
+        console.log(program);
+        console.log('ALLO4');
+        //let musicTitle = self.findBroadcastingTitle(found.musicTracks);
+        let broadcastTwo = found.musicTracks.find(function(item) {
+          return dayjs(item.broadcastedAt).unix() <= now && dayjs(item.broadcastedLastTimeAt).unix() > now;
+        });
+        console.log(broadcastTwo);
+        let musicTitle = {};
+        if (broadcastTwo) {
+          musicTitle = {
+            title: broadcastTwo.title,
+            artist: broadcastTwo.artists,
+            startTime: dayjs(broadcastTwo.broadcastedAt).unix(),
+            endTime: dayjs(broadcastTwo.broadcastedLastTime).unix(),
+          };
+        }
+        console.log(musicTitle)
+        console.log('ALLO5');
+        //let nextTitleStartTime = self.findNextBroadcastingStartTime(found.musicTracks);
+        let broadcastThree = found.musicTracks.find(function(item) {
+          return dayjs(item.broadcastedAt).unix() >= now;
+        });
+        let nextTitleStartTime = null;
+        if (broadcastThree) {
+          nextTitleStartTime = dayjs(broadcastThree.broadcastedAt).unix();
+        }
+        console.log(nextTitleStartTime);
+        console.log('ALLO6');
+        let scraped = {...program, ...musicTitle, ...{endTime : Math.min(program.endTime,nextTitleStartTime)}};
+        console.log('ALLO7');
+        console.log(scraped);
+        return scraped;
       });
+  }
+
+  findBroadcastingProgram(broadcasts) {
+    let now = dayjs().unix();
+    let broadcast = broadcasts.find(function(item) {
+      return dayjs(item.startsAt).unix() <= now && dayjs(item.endsAt).unix() > now;
+    });
+    if (!broadcast) {return {};}
+    return {
+      title: broadcast.title,
+      artist: broadcast.credits,
+      cover: broadcast.picture.url.replace('{1}','1x1').replace('{0}','400'),
+      startTime: dayjs(broadcast.startsAt).unix(),
+      endTime: dayjs(broadcast.endsAt).unix(),
+    }
   }
 
   findBroadcastingTitle(broadcasts) {
@@ -52,16 +95,21 @@ class RCIciMusiqueScraper extends BaseScraper {
       return dayjs(item.broadcastedAt).unix() <= now && dayjs(item.broadcastedLastTimeAt).unix() > now;
     });
     if (!broadcast) {return {};}
-    return broadcast;
+    return {
+      title: broadcast.title,
+      artist: broadcast.artists,
+      startTime: dayjs(broadcast.broadcastedAt).unix(),
+      endTime: dayjs(broadcast.broadcastedLastTime).unix(),
+    };
   }
 
-  findNextBroadcastingTitle(broadcasts) {
+  findNextBroadcastingStartTime(broadcasts) {
     let now = dayjs().unix();
     let broadcast = broadcasts.find(function(item) {
       return dayjs(item.broadcastedAt).unix() >= now;
     });
-    if (!broadcast) {return {};}
-    return broadcast;
+    if (!broadcast) {return null;}
+    return dayjs(broadcast.broadcastedAt).unix();
   }
 
 }
--- metaradio/scrapers/r_c_ici_musique_classique.js
+++ metaradio/scrapers/r_c_ici_musique_classique.js
@@ -16,10 +16,100 @@ class RCIciMusiqueClassiqueScraper extends BaseScraper {
         }
       })
       .then(function (metadata) {
-        return {
-          title: 'ICI Musique Classique'
+        var self = this;
+        console.log('ALLO');
+        let found = metadata.schedules.find(item => item.broadcastingNetworkId == 16 && item.broadcastingStationId == 111);
+        if (!found) {console.log('ALLO2');return {};}
+        console.log('ALLO3');
+        //let program = self.findBroadcastingProgram(found.trafficBroadcasts);
+        dayjs.extend(utc);
+        console.log('ALLO3-00');
+        let now = dayjs().unix();
+        console.log('ALLO3-01');
+        let broadcast = found.trafficBroadcasts.find(function(item) {
+          console.log('HEY');
+          return dayjs(item.startsAt).unix() <= now && dayjs(item.endsAt).unix() > now;
+        });
+        console.log('ALLO3-1');
+        if (!broadcast) {return {};}
+        let program = {
+          title: broadcast.title,
+          artist: broadcast.credits,
+          cover: broadcast.picture.url.replace('{1}','1x1').replace('{0}','400'),
+          startTime: dayjs(broadcast.startsAt).unix(),
+          endTime: dayjs(broadcast.endsAt).unix(),
         };
+        console.log(program);
+        console.log('ALLO4');
+        //let musicTitle = self.findBroadcastingTitle(found.musicTracks);
+        let broadcastTwo = found.musicTracks.find(function(item) {
+          return dayjs(item.broadcastedAt).unix() <= now && dayjs(item.broadcastedLastTimeAt).unix() > now;
+        });
+        console.log(broadcastTwo);
+        let musicTitle = {};
+        if (broadcastTwo) {
+          musicTitle = {
+            title: broadcastTwo.title,
+            artist: broadcastTwo.artists,
+            startTime: dayjs(broadcastTwo.broadcastedAt).unix(),
+            endTime: dayjs(broadcastTwo.broadcastedLastTime).unix(),
+          };
+        }
+        console.log(musicTitle)
+        console.log('ALLO5');
+        //let nextTitleStartTime = self.findNextBroadcastingStartTime(found.musicTracks);
+        let broadcastThree = found.musicTracks.find(function(item) {
+          return dayjs(item.broadcastedAt).unix() >= now;
+        });
+        let nextTitleStartTime = null;
+        if (broadcastThree) {
+          nextTitleStartTime = dayjs(broadcastThree.broadcastedAt).unix();
+        }
+        console.log(nextTitleStartTime);
+        console.log('ALLO6');
+        let scraped = {...program, ...musicTitle, ...{endTime : Math.min(program.endTime,nextTitleStartTime)}};
+        console.log('ALLO7');
+        console.log(scraped);
+        return scraped;
       });
+  }
+
+  findBroadcastingProgram(broadcasts) {
+    let now = dayjs().unix();
+    let broadcast = broadcasts.find(function(item) {
+      return dayjs(item.startsAt).unix() <= now && dayjs(item.endsAt).unix() > now;
+    });
+    if (!broadcast) {return {};}
+    return {
+      title: broadcast.title,
+      artist: broadcast.credits,
+      cover: broadcast.picture.url.replace('{1}','1x1').replace('{0}','400'),
+      startTime: dayjs(broadcast.startsAt).unix(),
+      endTime: dayjs(broadcast.endsAt).unix(),
+    }
+  }
+
+  findBroadcastingTitle(broadcasts) {
+    let now = dayjs().unix();
+    let broadcast = broadcasts.find(function(item) {
+      return dayjs(item.broadcastedAt).unix() <= now && dayjs(item.broadcastedLastTimeAt).unix() > now;
+    });
+    if (!broadcast) {return {};}
+    return {
+      title: broadcast.title,
+      artist: broadcast.artists,
+      startTime: dayjs(broadcast.broadcastedAt).unix(),
+      endTime: dayjs(broadcast.broadcastedLastTime).unix(),
+    };
+  }
+
+  findNextBroadcastingStartTime(broadcasts) {
+    let now = dayjs().unix();
+    let broadcast = broadcasts.find(function(item) {
+      return dayjs(item.broadcastedAt).unix() >= now;
+    });
+    if (!broadcast) {return null;}
+    return dayjs(broadcast.broadcastedAt).unix();
   }
 
 }
