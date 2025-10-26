0cda7f5121f45ee523948e22f03be0b0203a8bed
volumio <d.bourcet@gmail.com>
2025-10-26 00:42:38 -0400

    Fix Radio Nova webradios

---
+++ metaradio/scrapers/radio_nova_classics.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const jp = require('jsonpath');
+const BaseScraper = require('./base');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+
+class RadioNovaClassicsScraper extends BaseScraper {
+
+  _scrapeMetadata(response) {
+    const metadata = JSON.parse(response);
+    const [title] = jp.query(metadata, '$[6].currentTrack.title');
+    const [artist] = jp.query(metadata, '$[6].currentTrack.artist');
+    const [cover] = jp.query(metadata, '$[6].currentTrack.image');
+    const [diffusionDate] = jp.query(metadata, '$[6].currentTrack.diffusion_date');
+    const [rawDuration] = jp.query(metadata, '$[6].currentTrack.duration');
+    dayjs.extend(utc);
+    dayjs.extend(timezone);
+    const startTime = dayjs.tz(diffusionDate, 'Europe/Paris').unix();
+    const [minutes,seconds] = rawDuration.split(':');
+    const endTime = startTime + minutes * 60 + (+seconds);
+
+    return {
+      title,
+      artist,
+      cover,
+      startTime,
+      endTime,
+    };
+  }
+
+}
+
+module.exports = RadioNovaClassicsScraper;
